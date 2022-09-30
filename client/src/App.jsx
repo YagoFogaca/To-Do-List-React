21d0f342c2d050144e2a315a72e50cd534685c66
Yago Fogaca <yagofogaca20@gmail.com>
2022-09-30 18:28:28 -0300

    feat: test repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@ import { Initial } from "./page/initial/initial";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export function App() {
-  // Aqui vai ficar somente <Login / > e <Tasks />
   return (
     <BrowserRouter>
       <Routes>
