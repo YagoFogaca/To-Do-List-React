0b429cc0f540373f8602881d8fbe42ca65b733f6
Yago Fogaca <yagofogaca20@gmail.com>
2022-09-30 19:57:30 -0300

    fix: remove Comments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,10 @@
 import "./reset.css";
 
-// import { Body } from "./components/body/body";
 import { Home } from "./page/home/home";
 import { Initial } from "./page/initial/initial";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export function App() {
-  // APENAS UM TESTE
   return (
     <BrowserRouter>
       <Routes>
